66a270e6f54c550d20e2b405e7ff12f8ab06cacc
GitNes26 <nestorpuentesin@gmail.com>
2022-08-16 22:57:24 -0500

    Se modifico la ruta de Plantilla.json

--- N2/main.py
+++ N2/main.py
@@ -4,9 +4,8 @@ from classelenium import Selenium
 from objPlantilla import ObjElement
 from objPlantilla import ObjPlantilla
 
-df = pd.read_csv(r'D:\UTT TIDSM\9no ING\AdministracionBD\Selenium\UNIDAD_5_TORO\N2\Plantilla.csv')
 sl = Selenium()
-path_file = r'D:\UTT TIDSM\9no ING\AdministracionBD\Selenium\UNIDAD_5_TORO\N2\Plantilla.json'
+path_file = r'Plantilla.json'
 
 objList = ObjPlantilla().GetObjects(path_file)
 for obj in objList:
@@ -20,29 +19,3 @@ for obj in objList:
     drive.close()
     print("FIN.")
     pass
-
-
-
-
-
-# drive = sl.navegador(df['Navegador'][0])
-# cont = 0
-# all=df['Path_Formulario_Input'].count()
-# sl.ventana(drive, df['Url'][0])
-# while(all>cont):
-#     sl.write_Input(drive,df['Path_Formulario_Input'][cont],df['Valor_Input'][cont])
-#     cont +=1
-# all=df['Path_Formulario_Select'].count()
-# cont = 0
-# while(all>cont):
-#     sl.write_Select(drive,df['Path_Formulario_Select'][cont],df['Valor_Select'][cont])
-#     cont +=1
-# all=df['Path_Formulario_Checkbox'].count()
-# cont = 0
-# while(all>cont):
-#     sl.write_Checkbox(drive,df['Path_Formulario_Checkbox'][cont])
-#     cont +=1
-
-# time.sleep(3)
-# sl.save(drive,df["Guardar_Botton"][0])
-# drive.close()
